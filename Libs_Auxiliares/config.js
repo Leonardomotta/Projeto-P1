98c7652043145aba3d89a33ebac117f124a3d1ad
Mota <leonardo.r.mota@accenture.com>
2019-04-08 15:12:51 -0300

    config do banco

--- Libs_Auxiliares/config.js
+++ Libs_Auxiliares/config.js
@@ -1,7 +1,14 @@
+let mongoose = require('mongoose');
+
 config = {};
 
-config.port = (process.env.port || 3000 ); 
+// ATENÇÃO  senha deve ser passada como paramentro ao executar a aplicação // 
 
+config.senha =  process.argv0;
+config.port = (process.env.port || 3000 ); 
+config.conection_string = "mongodb+srv://admin:"+config.senha+"@cluster0-rztiw.mongodb.net/test?retryWrites=true"
+config.DB = mongoose
+config.DB.connect(config.conection_string);
 
 
 
--- main.js
+++ main.js
@@ -4,7 +4,6 @@ http = require('http');
 
 
 http.createServer(rm);
-console.log(config.port)
 
 
 
