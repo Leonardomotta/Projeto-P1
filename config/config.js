dbaafe3204e668bea356712bc0b5983dabbb35d7
unknown <pedro.braga@ccc.ufcg.edu.br>
2019-04-08 23:00:01 -0300

    Small modifications

--- config/config.js
+++ config/config.js
@@ -4,8 +4,8 @@ config = {};
 
 // ATENÇÃO  senha deve ser passada como paramentro ao executar a aplicação // 
 
-config.senha =  process.argv[2];
-config.port = (process.env.port || 3000 ); 
+config.senha =  process.env.senha || process.argv[2];
+config.port = (process.env.PORT || 3000 ); 
 config.conection_string = "mongodb+srv://admin:"+config.senha+"@cluster0-rztiw.mongodb.net/test?retryWrites=true";
 config.DB = mongoose;
 config.DB.connect(config.conection_string , {useNewUrlParser: true})
