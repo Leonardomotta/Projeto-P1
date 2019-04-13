33226c1c3d2a2118dfc957204b23c6414d9518f8
unknown <pedro.braga@ccc.ufcg.edu.br>
2019-04-13 03:11:58 -0300

    Adiciona middleware para autorizacao

--- routes/routerManager.js
+++ routes/routerManager.js
@@ -1,12 +1,13 @@
 express = require('express');
 users = require('./users');
 auth = require('./auth')
+authMiddleware = require('../middlewares/authorization')
 rm = express();
 var bodyParser = require('body-parser');
 
 rm.use(bodyParser.json());
 
-rm.use('/status', (req, res, next) => {
+rm.use('/status', authMiddleware, (req, res, next) => {
     res.json({status: 'Server running!'})
 })
 
