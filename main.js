8fddabd0d0989927cb0b5f389451003d19b4fa5b
unknown <pedro.braga@ccc.ufcg.edu.br>
2019-05-01 00:42:51 -0300

    Implementa API de posts

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ http = require('http');
 mongoose = require('mongoose');
 
 rm.use("/profile_images/", express.static(__dirname + '/uploads/users/profile_images'));
+rm.use("/post_images/", express.static(__dirname + '/uploads/posts/posts_images'))
 
 
 http.createServer(rm);
